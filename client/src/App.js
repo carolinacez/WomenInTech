085e96cd0a5326f3bcb1a100c6f0e7d02467ce76
carolina <carolinaccez@gmail.com>
2020-11-07 12:57:55 -0500

    deploy1

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import shirt from './logo_girl_code.png'
+// import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+      <img src={shirt} className="App-logo" alt="logo" />
+        {/* <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
@@ -16,7 +17,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
