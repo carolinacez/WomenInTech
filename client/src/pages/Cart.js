266d18fe0a5d3c148ed588cebcf34011da3575b5
carolina <carolinaccez@gmail.com>
2020-11-10 23:06:37 -0500

    added function that adds total prices of items in the cart

--- client/src/pages/Cart.js
+++ client/src/pages/Cart.js
@@ -1,28 +1,49 @@
-import React from 'react'; 
+import React, {useEffect} from 'react';
 
-function Cart({cart, removeFromCart}) { 
+function Cart({ cart, removeFromCart, addToCart }) {
     console.log(cart)
+
+
+    useEffect(() => {
+     
+        if(cart.length > 0) {
+            // console.log(cart.length)
+        var sum = 0;
+
+        for (var i = 0; i < cart.length; i++) {
+            // console.log(cart[i].price )
+              sum += (cart[i].price) 
+            // sum += parseInt(cart.price)
+            // console.log(sum)
+            document.title = `Your cart total is ${sum}`;
+        }
+        } else { 
+            console.log('nothing in cart')
+        }
+        
+
+    }, [cart, addToCart]) 
     
 
-
-    const cartItems = cart && cart.length ? ( cart.map((product, i) => (
+    const cartItems = cart && cart.length ? (cart.map((product, i) => (
         <div key={`${product._id} ${i}`}>
-                <img
+            <img
                 alt={product.name}
                 src={`/images/${product.image}`} />
-                 <p>{product.name}</p>
-                 <input type="submit" value='remove' onClick={()=> removeFromCart(product)}></input>
+            <p>{product.name}</p>
+            <input type="submit" value='remove' onClick={() => removeFromCart(product)}></input>
 
-                 <div>
-                     <strong> Total: ${product.price}</strong>
-                 </div>
+            <div>
+    <strong> Total:{product.price}</strong>
+            </div>
         </div>
     ))) : <span></span>
     return (
-       
-       
-       <div>
-{cartItems}
+
+
+        <div>
+            {cartItems}
+            
 
         </div>
     )
