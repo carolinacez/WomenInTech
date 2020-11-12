30cc21829089c31621ba7adb8583c418427a343e
allielynne20 <allielynne20@gmail.com>
2020-11-11 23:00:26 -0500

    styled homepgae

--- client/src/App.js
+++ client/src/App.js
@@ -1,64 +1,76 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { ApolloProvider } from '@apollo/react-hooks';
-import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from "@apollo/react-hooks";
+import ApolloClient from "apollo-boost";
 // import logo from './logo_girl_code.png';
 import Nav from "./components/Nav";
 import Aboutus from "./pages/Aboutus";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Cart from "./pages/Cart";
-import Products from "./components/Products"
-
+import Products from "./components/Products";
 
 const client = new ApolloClient({
   request: (operation) => {
-    const token = localStorage.getItem('id_token')
+    const token = localStorage.getItem("id_token");
     operation.setContext({
       headers: {
-        authorization: token ? `Bearer ${token}` : ''
-      }
-    })
+        authorization: token ? `Bearer ${token}` : "",
+      },
+    });
   },
-  uri: process.env.NODE_ENV === 'production' ? '/graphql' : 'http://localhost:3001/graphql',
-})
-
+  uri:
+    process.env.NODE_ENV === "production"
+      ? "/graphql"
+      : "http://localhost:3001/graphql",
+});
 
 function App() {
-
-  const [cart, setCart] = useState([])
+  const [cart, setCart] = useState([]);
 
   const addToCart = (item) => {
     setCart([...cart, item]);
-    console.log(item); 
-    }
-  
+    console.log(item);
+  };
+
   const removeFromCart = (product) => {
-    let copy = [...cart]
-    copy = copy.filter(cartItem => cartItem._id !== product._id)
-    setCart(copy); 
-}
+    let copy = [...cart];
+    copy = copy.filter((cartItem) => cartItem._id !== product._id);
+    setCart(copy);
+  };
 
   return (
     <ApolloProvider client={client}>
-    <Router>
-      <Nav cart={cart}/>
-      <Switch>
-       <Route exact path="/" component={Aboutus} /> 
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/signup" component={Signup} />
-        <Route exact path="/cart" component={() => <Cart cart={cart} setCart={setCart} removeFromCart={removeFromCart} />}  /> 
-        <Route exact path ="/products" component={()=> <Products cart={cart} addToCart={addToCart} />} /> 
-      <div className="App">
-      <header className="App-header">
-   
-
-      </header>
-    </div>
-    </Switch>
-    </Router>
+      <Router>
+        <Nav cart={cart} />
+        <Switch>
+          <Route exact path="/" component={Aboutus} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/signup" component={Signup} />
+          <Route
+            exact
+            path="/cart"
+            component={() => (
+              <Cart
+                cart={cart}
+                setCart={setCart}
+                removeFromCart={removeFromCart}
+              />
+            )}
+          />
+          <Route
+            exact
+            path="/products"
+            component={() => <Products cart={cart} addToCart={addToCart} />}
+          />
+          <div className="App">
+            <header className="App-header"></header>
+            <body></body>
+          </div>
+        </Switch>
+      </Router>
     </ApolloProvider>
   );
-      }
+}
 
 export default App;
--- client/src/components/Products/index.js
+++ client/src/components/Products/index.js
@@ -1,50 +1,49 @@
-import React, {useState, useEffect} from "react";
-import { QUERY_PRODUCTS} from '../../utils/queries'
-import { useQuery } from '@apollo/react-hooks';
+import React, { useState, useEffect } from "react";
+import { QUERY_PRODUCTS } from "../../utils/queries";
+import { useQuery } from "@apollo/react-hooks";
 import { Link } from "react-router-dom";
-import Auth from '../../utils/auth';
+import Auth from "../../utils/auth";
 
+function ProductList({ addToCart }) {
+  const { data } = useQuery(QUERY_PRODUCTS);
+  const products = data && data.products ? data.products : [];
 
-function ProductList({addToCart}) {
-
-
-    const { data } = useQuery(QUERY_PRODUCTS)
-    const products = data && data.products ? data.products : []
-
-
-    function listItems(){ 
-        if(Auth.loggedIn()) {
-            return(
-        products.map((product, i) => (
+  function listItems() {
+    if (Auth.loggedIn()) {
+      return products.map((product, i) => (
         <div key={`${i}`}>
-                <img
-                alt={product.name}
-                src={`/images/${product.image}`} />
-                 <p>{product.name}</p>
-                <Link to={'/cart'}><input type="submit" value='add' onClick={()=> addToCart(product)}></input></Link> 
-              
+          <div>
+            <img alt={product.name} src={`/images/${product.image}`} />
+          </div>
+          <p>{product.name}</p>
+          <Link to={"/cart"}>
+            <input
+              type="submit"
+              value="add"
+              onClick={() => addToCart(product)}
+            ></input>
+          </Link>
         </div>
-    )))} else {
-        return(
-            products.map((product, i) => (
-                <div key={`${i}`}>
-                    <img alt={product.name} src={`/images/${product.image}`} />
-            <p>{product.name}</p>
-            <p>Please log in or signup to add items to cart!</p>
-                </div>
-            ))
-        )
-    }}
+      ));
+    } else {
+      return products.map((product, i) => (
+        <div key={`${i}`}>
+          <img alt={product.name} src={`/images/${product.image}`} />
+          <p>{product.name}</p>
+          <p>Please log in or signup to add items to cart!</p>
+        </div>
+      ));
+    }
+  }
 
-
-
-    return (
-        <div>            
-            <h2>Merch:</h2>
-        <div>{listItems()}</div>
-        </div> 
-)
+  return (
+    <div>
+      <div className="background-color">
+        <h1 className="merch-title">Merch:</h1>
+      </div>
+      <div>{listItems()}</div>
+    </div>
+  );
 }
 
-export default ProductList; 
-
+export default ProductList;
